135e3fa07bb5670784f348d0a400f974b5f56050
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-10-21 09:25:17 -0500

    refactoring

--- optimized/prime_factors_including_tail.rb
+++ optimized/prime_factors_including_tail.rb
@@ -14,14 +14,10 @@ def is_prime?(number)
 end
 
 def prime_factors(number, i = 2, primes = [])
-  if number % i == 0
-    if primes.include?(i) || is_prime?(i)
-      primes << i
-      return primes if i == number
-      prime_factors(number / i, i, primes)
-    else
-      prime_factors(number, i + 1, primes)
-    end
+  if number % i == 0 && (primes.include?(i) || is_prime?(i))
+    primes << i
+    return primes if i == number
+    prime_factors(number / i, i, primes)
   else
     prime_factors(number, i + 1, primes)
   end
